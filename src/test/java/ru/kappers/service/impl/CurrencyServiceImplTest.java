2f272b8802915fbf4ae136ddf722a89b1e48747a
Ruslan Suleymanov <rusdenis82@gmail.com>
2026-03-05 02:31:18 +0300

    Добавлен TODO

--- src/test/java/ru/kappers/service/impl/CurrencyServiceImplTest.java
+++ src/test/java/ru/kappers/service/impl/CurrencyServiceImplTest.java
@@ -22,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
+//todo Досмотреть тесты и исходный класс
 public class CurrencyServiceImplTest extends UnitTest {
     @InjectMocks
     private CurrencyServiceImpl currencyService;
