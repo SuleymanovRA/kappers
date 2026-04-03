970773445ef286448e0f1424cff46333665cb618
Ruslan Suleymanov <rusdenis82@gmail.com>
2026-04-03 20:40:32 +0300

    Рефакторинг `EventToEventDTOConverter`

--- src/main/java/ru/kappers/convert/EventToEventDTOConverter.java
+++ src/main/java/ru/kappers/convert/EventToEventDTOConverter.java
@@ -1,33 +1,15 @@
 package ru.kappers.convert;
 
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Service;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.kappers.model.Event;
 import ru.kappers.model.dto.EventDTO;
 
-import java.util.Objects;
+import static org.mapstruct.NullValueMappingStrategy.RETURN_DEFAULT;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.util.Objects.nonNull;
-
-@Service
-public class EventToEventDTOConverter implements Converter<Event, EventDTO> {
+@Mapper(componentModel = "spring", nullValueMappingStrategy = RETURN_DEFAULT)
+public abstract class EventToEventDTOConverter extends BaseMapStructConverter<Event, EventDTO> {
+    @Mapping(target = "f_id", expression = "java(source.getFixture() != null ? source.getFixture().getId() : EventDTO.EMPTY_FIXTURE_ID)")
     @Override
-    public EventDTO convert(Event source) {
-        checkArgument(nonNull(source), "source must not null");
-        return EventDTO.builder()
-                .f_id(getFixtureIdOrEmpty(source))
-                .outcome(source.getOutcome())
-                .coefficient(source.getCoefficient())
-                .tokens(source.getTokens())
-                .price(source.getPrice())
-                .build();
-    }
-
-    private int getFixtureIdOrEmpty(Event source) {
-        if (Objects.isNull(source.getFixture())) {
-            return EventDTO.EMPTY_FIXTURE_ID;
-        }
-        return source.getFixture().getId();
-    }
+    public abstract EventDTO convert(Event source);
 }
