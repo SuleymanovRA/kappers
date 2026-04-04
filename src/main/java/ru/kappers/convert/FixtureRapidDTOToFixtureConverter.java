02ffc01bd77a077146d438c689082336531d71a9
Ruslan Suleymanov <rusdenis82@gmail.com>
2026-04-04 12:28:48 +0300

    Рефакторинг `FixtureRapidDTOToFixtureConverter` (#100)
    - Рефакторинг `FixtureRapidDTOToFixtureConverter`.
    - Рефакторинг `FixtureRapidDTOToFixtureConverterTest`.
    - Доработан `EventToEventDTOConverterTest`, добавлен тест для null исходного объекта.

--- src/main/java/ru/kappers/convert/FixtureRapidDTOToFixtureConverter.java
+++ src/main/java/ru/kappers/convert/FixtureRapidDTOToFixtureConverter.java
@@ -1,46 +1,28 @@
 package ru.kappers.convert;
 
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.stereotype.Service;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.kappers.model.Fixture;
 import ru.kappers.model.dto.rapidapi.FixtureRapidDTO;
-import ru.kappers.util.DateTimeUtil;
 
-import javax.annotation.Nullable;
+import static org.mapstruct.NullValueMappingStrategy.RETURN_DEFAULT;
 
 /**
  * Конвертер из {@link FixtureRapidDTO} в {@link Fixture}
  */
-@Service
-public class FixtureRapidDTOToFixtureConverter implements Converter<FixtureRapidDTO, Fixture> {
-
-    @Nullable
+@Mapper(componentModel = "spring", nullValueMappingStrategy = RETURN_DEFAULT)
+public abstract class FixtureRapidDTOToFixtureConverter extends BaseMapStructConverter<FixtureRapidDTO, Fixture> {
+    @Mapping(source = "fixture_id", target = "id")
+    @Mapping(source = "event_timestamp", target = "eventTimestamp")
+    @Mapping(target = "eventDate", expression = "java(ru.kappers.util.DateTimeUtil.parseLocalDateTimeFromZonedDateTime(source.getEvent_date()))")
+    @Mapping(source = "league_id", target = "leagueId")
+    @Mapping(source = "homeTeam_id", target = "homeTeamId")
+    @Mapping(source = "awayTeam_id", target = "awayTeamId")
+    @Mapping(target = "status", expression = "java(Fixture.Status.byValue(source.getStatus()))")
+    @Mapping(target = "statusShort", expression = "java(Fixture.ShortStatus.byValue(source.getStatusShort()))")
+    @Mapping(source = "halftime_score", target = "halftimeScore")
+    @Mapping(source = "final_score", target = "finalScore")
+    @Mapping(target = "events", ignore = true)
     @Override
-    public Fixture convert(@Nullable FixtureRapidDTO dto) {
-        if (dto == null) {
-            return null;
-        }
-        final Fixture fixture = Fixture.builder()
-            .id(dto.getFixture_id())
-            .eventTimestamp(dto.getEvent_timestamp())
-            .eventDate(DateTimeUtil.parseLocalDateTimeFromZonedDateTime(dto.getEvent_date()))
-            .leagueId(dto.getLeague_id())
-            .round(dto.getRound())
-            .homeTeamId(dto.getHomeTeam_id())
-            .awayTeamId(dto.getAwayTeam_id())
-            .homeTeam(dto.getHomeTeam())
-            .awayTeam(dto.getAwayTeam())
-            .status(Fixture.Status.byValue(dto.getStatus()))
-            .statusShort(Fixture.ShortStatus.byValue(dto.getStatusShort()))
-            .goalsHomeTeam(dto.getGoalsHomeTeam())
-            .goalsAwayTeam(dto.getGoalsAwayTeam())
-            .halftimeScore(dto.getHalftime_score())
-            .finalScore(dto.getFinal_score())
-            .penalty(dto.getPenalty())
-            .elapsed(dto.getElapsed())
-            .firstHalfStart(dto.getFirstHalfStart())
-            .secondHalfStart(dto.getSecondHalfStart())
-            .build();
-        return fixture;
-    }
+    public abstract Fixture convert(FixtureRapidDTO source);
 }
--- src/test/java/ru/kappers/convert/FixtureRapidDTOToFixtureConverterTest.java
+++ src/test/java/ru/kappers/convert/FixtureRapidDTOToFixtureConverterTest.java
@@ -1,101 +1,74 @@
 package ru.kappers.convert;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
+import ru.kappers.UnitTest;
 import ru.kappers.model.Fixture;
 import ru.kappers.model.dto.rapidapi.FixtureRapidDTO;
 import ru.kappers.util.DateTimeUtil;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.instancio.Select.field;
 
-@RunWith(MockitoJUnitRunner.class)
-public class FixtureRapidDTOToFixtureConverterTest {
-
+class FixtureRapidDTOToFixtureConverterTest extends UnitTest {
     @InjectMocks
-    private FixtureRapidDTOToFixtureConverter converter;
+    private FixtureRapidDTOToFixtureConverterImpl converter;
 
     @Test
-    public void convertMustReturnNullIfParameterIsNull() {
-        assertNull(converter.convert(null));
+    void convertMustThrowExceptionIfParameterIsNull() {
+        assertThatThrownBy(() -> converter.convert(null))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    public void convert() {
-        final List<FixtureRapidDTO> dtoList = Arrays.asList(
-                FixtureRapidDTO.builder()
-                        .fixture_id(1)
-                        .event_timestamp(1L)
-                        .event_date("2020-12-02T10:15:30+01:00")
-                        .league_id(1)
-                        .round("0")
-                        .homeTeam_id(1)
-                        .awayTeam_id(2)
-                        .homeTeam("ЦСК")
-                        .awayTeam("Спартак")
-                        .status(Fixture.Status.NOT_STARTED.getValue())
-                        .statusShort(Fixture.ShortStatus.NOT_STARTED.getValue())
-                        .goalsHomeTeam(0)
-                        .goalsAwayTeam(0)
-                        .halftime_score("")
-                        .final_score("")
-                        .penalty("")
-                        .elapsed(0)
-                        .firstHalfStart(0L)
-                        .secondHalfStart(0L)
-                        .build(),
-                FixtureRapidDTO.builder()
-                        .fixture_id(2)
-                        .event_timestamp(2L)
-                        .event_date("2019-03-02T10:15:30+01:00")
-                        .league_id(2)
-                        .round("1")
-                        .homeTeam_id(3)
-                        .awayTeam_id(4)
-                        .homeTeam("Факел")
-                        .awayTeam("Динамо")
-                        .status(Fixture.Status.MATCH_FINISHED.getValue())
-                        .statusShort(Fixture.ShortStatus.MATCH_FINISHED.getValue())
-                        .goalsHomeTeam(1)
-                        .goalsAwayTeam(2)
-                        .halftime_score("0:1")
-                        .final_score("1:2")
-                        .penalty("1")
-                        .elapsed(1)
-                        .firstHalfStart(1L)
-                        .secondHalfStart(2L)
-                        .build()
-        );
-
-        for (FixtureRapidDTO dto : dtoList) {
-            final Fixture fixture = converter.convert(dto);
-
-            assertThat(fixture, is(notNullValue()));
-            assertThat(fixture.getId(), is(dto.getFixture_id()));
-            assertThat(fixture.getEventTimestamp(), is(dto.getEvent_timestamp()));
-            assertThat(fixture.getEventDate(), is(DateTimeUtil.parseLocalDateTimeFromZonedDateTime(dto.getEvent_date())));
-            assertThat(fixture.getLeagueId(), is(dto.getLeague_id()));
-            assertThat(fixture.getRound(), is(dto.getRound()));
-            assertThat(fixture.getHomeTeamId(), is(dto.getHomeTeam_id()));
-            assertThat(fixture.getAwayTeamId(), is(dto.getAwayTeam_id()));
-            assertThat(fixture.getHomeTeam(), is(dto.getHomeTeam()));
-            assertThat(fixture.getAwayTeam(), is(dto.getAwayTeam()));
-            assertThat(fixture.getStatus(), is(Fixture.Status.byValue(dto.getStatus())));
-            assertThat(fixture.getStatusShort(), is(Fixture.ShortStatus.byValue(dto.getStatusShort())));
-            assertThat(fixture.getGoalsHomeTeam(), is(dto.getGoalsHomeTeam()));
-            assertThat(fixture.getGoalsAwayTeam(), is(dto.getGoalsAwayTeam()));
-            assertThat(fixture.getHalftimeScore(), is(dto.getHalftime_score()));
-            assertThat(fixture.getFinalScore(), is(dto.getFinal_score()));
-            assertThat(fixture.getPenalty(), is(dto.getPenalty()));
-            assertThat(fixture.getElapsed(), is(dto.getElapsed()));
-            assertThat(fixture.getFirstHalfStart(), is(dto.getFirstHalfStart()));
-            assertThat(fixture.getSecondHalfStart(), is(dto.getSecondHalfStart()));
+    void convert() {
+        for (FixtureRapidDTO fixtureRapidDTO : generatedFixtureRapidDTOList()) {
+            final Fixture fixture = converter.convert(fixtureRapidDTO);
+            assertThat(fixture)
+                    .isNotNull()
+                    .usingRecursiveComparison()
+                    .ignoringFields("id", "eventTimestamp", "eventDate", "leagueId", "homeTeamId",
+                            "awayTeamId", "status", "statusShort", "halftimeScore", "finalScore", "events")
+                    .isEqualTo(fixtureRapidDTO);
+            assertNotByFieldNames(fixture, fixtureRapidDTO);
         }
+    }
+
+    private List<FixtureRapidDTO> generatedFixtureRapidDTOList() {
+        return Instancio.ofList(FixtureRapidDTO.class)
+                .size(2)
+                .set(field(FixtureRapidDTO::getEvent_date), "2020-12-02T10:15:30+01:00")
+                .create();
+    }
+
+    private void assertNotByFieldNames(Fixture fixture, FixtureRapidDTO fixtureRapidDTO) {
+        assertThat(fixture)
+                .extracting(
+                        Fixture::getId,
+                        Fixture::getEventTimestamp,
+                        Fixture::getEventDate,
+                        Fixture::getLeagueId,
+                        Fixture::getHomeTeamId,
+                        Fixture::getAwayTeamId,
+                        Fixture::getStatus,
+                        Fixture::getStatusShort,
+                        Fixture::getHalftimeScore,
+                        Fixture::getFinalScore
+                ).containsExactly(
+                        fixtureRapidDTO.getFixture_id(),
+                        fixtureRapidDTO.getEvent_timestamp(),
+                        DateTimeUtil.parseLocalDateTimeFromZonedDateTime(fixtureRapidDTO.getEvent_date()),
+                        fixtureRapidDTO.getLeague_id(),
+                        fixtureRapidDTO.getHomeTeam_id(),
+                        fixtureRapidDTO.getAwayTeam_id(),
+                        Fixture.Status.byValue(fixtureRapidDTO.getStatus()),
+                        Fixture.ShortStatus.byValue(fixtureRapidDTO.getStatusShort()),
+                        fixtureRapidDTO.getHalftime_score(),
+                        fixtureRapidDTO.getFinal_score()
+                );
     }
 }
